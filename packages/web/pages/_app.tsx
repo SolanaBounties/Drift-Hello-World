4f711533ceb0d754c75b59737b211afcbb470129
joey <jkokinda9@gmail.com>
2025-09-26 00:20:22 -0400

    skeleton

---
+++ packages/web/pages/_app.tsx
@@ -0,0 +1,9 @@
+// TODO: Import wallet adapter providers
+// TODO: Set up Solana connection (devnet)
+// TODO: Configure wallet adapters (Phantom, Solflare, etc.)
+
+export default function App({ Component, pageProps }) {
+  // TODO: Set up wallet providers here
+  
+  return <Component {...pageProps} />
+}
---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,28 @@
+// TODO: Import Drift SDK here
+// TODO: Import wallet adapter hooks
+
+export default function Home() {
+  // TODO: Set up wallet connection
+  // TODO: Set up Drift client connection
+  // TODO: Add state for user account info
+
+  // TODO: Create function to initialize Drift client
+  // TODO: Create function to get user account
+  // TODO: Create function to display user balance
+
+  return (
+    <div>
+      <h1>Drift Hello Worlddddddddddd</h1>
+      
+      {/* TODO: Add wallet connect button */}
+      
+      {/* TODO: Show wallet status when connected */}
+      
+      {/* TODO: Add button to connect to Drift */}
+      
+      {/* TODO: Display user account info */}
+      
+      {/* TODO: Add trading interface */}
+    </div>
+  )
+}
