0f5a3d6d296e91c47876a4b7d538832dc2e7feb2
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-10-02 16:42:49 -0400

    Remove rust files

--- packages/web/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// TODO: Import wallet adapter providers
-// TODO: Set up Solana connection (devnet)
-// TODO: Configure wallet adapters (Phantom, Solflare, etc.)
-
-export default function App({ Component, pageProps }) {
-  // TODO: Set up wallet providers here
-  
-  return <Component {...pageProps} />
-}
--- programs/drift-hello-world/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// TODO: Add Drift Protocol imports here
-// TODO: Add your program ID
-
-// TODO: Create a function that interacts with Drift
-// - Initialize a user account
-// - Get user balance
-// - Place a trade
-
-// TODO: Define the accounts structure for Drift interaction
